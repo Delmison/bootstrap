640e5e3af201c7ecf5c748bb2f2a3ebf2e0fbd9c
Delmison Seidel <55005824+Delmison@users.noreply.github.com>
2022-11-18 17:50:37 -0300

    adicionado navegaco nas paginas

---
+++ javascript/index.js
@@ -0,0 +1,11 @@
+function servico(){
+    window.location.href="servicos.html"
+}
+
+function home(){
+    window.location.href="index.html"
+}
+
+function parceiros(){
+    window.location.href="parceiros.html"
+}
