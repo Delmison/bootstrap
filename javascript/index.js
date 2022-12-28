8419cbf69733dfad19a8491a73936eea75d4c619
Delmison Seidel <55005824+Delmison@users.noreply.github.com>
2022-12-28 17:36:08 -0300

    colocado opção de recuperar senha

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,40 @@
+function onChangeEmail() {
+    toggleButtonsDisable();
+    toggleEmailErrors();
+}
+
+function onChangePassword() {
+    toggleButtonsDisable();
+    togglePasswordErrors();
+}
+
+function login() {
+    
+    firebase.auth().signInWithEmailAndPassword(
+        form.usuario().value, form.senha().value
+    ).then(response => {
+        window.location.href = "administrador.html";
+    }).catch(error => {
+        alert(GetErrorMenssage(error));
+    });
+}
+
+function recoverPassword() {
+    firebase.auth().sendPasswordResetEmail(form.usuario().value).then(() => {
+        alert('Email enviado com sucesso');
+    }).catch(error => {
+        alert('Email não encontrado');
+    });
+}
+
+function GetErrorMenssage(error){
+    if(error.code=="auth/user-not-found"){
+        return "Usuário não encontrado!";
+    }
+    return error.message;
+}
+
+
 function servico(){
     window.location.href="servicos.html"
 }
@@ -13,3 +50,21 @@ function parceiros(){
 function quemsomos(){
     window.location.href="quemsomos.html"
 }
+
+function arearestrita(){
+    window.location.href="login.html"
+}
+
+function cadastro(){
+    window.location.href="cadastro.html"
+}
+
+const form = {
+    usuario: () => document.getElementById("email"),
+    emailInvalidError: () => document.getElementById("email-invalid-error"),
+    emailRequiredError: () => document.getElementById("email-required-error"),
+    loginButton: () => document.getElementById("login-button"),
+    senha: () => document.getElementById("password"),
+    passwordRequiredError: () => document.getElementById("password-required-error"),
+    recoverPasswordButton: () => document.getElementById("recover-password-button"),
+} 
