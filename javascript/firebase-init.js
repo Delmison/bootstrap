7858bc88e8ca2019ed9a9c6f9cb50c4dcb933cbb
Delmison Seidel <55005824+Delmison@users.noreply.github.com>
2022-12-29 16:21:25 -0300

    validado o processo de cadastro

---
+++ javascript/firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDQ895UNZ2jCYai6Yo6uennk2Au_RRjwk4",
+    authDomain: "area-restrita-hendel.firebaseapp.com",
+    projectId: "area-restrita-hendel",
+    storageBucket: "area-restrita-hendel.appspot.com",
+    messagingSenderId: "1014328781875",
+    appId: "1:1014328781875:web:b1ff3f4ae75213dba18746"
+  };
+  firebase.initializeApp(firebaseConfig);
